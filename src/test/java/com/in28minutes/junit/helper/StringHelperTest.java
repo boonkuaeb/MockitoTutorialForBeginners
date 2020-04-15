b809f53428824edc972b85c95e8995b471a14027
BOONKUAE BOONSUTTA <boonkuae.boonsutta@scb.co.th>
2020-04-15 21:48:27 +0700

    Unit Testing with Junit: Step 04: Refactoring Your First JUnit Test

--- src/test/java/com/in28minutes/junit/helper/StringHelperTest.java
+++ src/test/java/com/in28minutes/junit/helper/StringHelperTest.java
@@ -1,18 +1,26 @@
 package com.in28minutes.junit.helper;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class StringHelperTest {
+    // Given
+    StringHelper helper = new StringHelper();
+
     // AACD => CD ACD => CD CDEF => CDEF CDAA =>CDAA
     @Test
-    void test()
-    {
-        // Given
-        StringHelper helper = new StringHelper();
+    @DisplayName("Truncate A in first two positions")
+    void testTruncateAInFirst2Positions_When_AInFirstTwoPositions() {
         // When and Assert
         assertEquals("CD", helper.truncateAInFirst2Positions("AACD"));
+    }
+
+    @Test
+    @DisplayName("Truncate A in first one position")
+    void testTruncateAInFirst2Positions_When_AInFirstPosition() {
+        // When and Assert
         assertEquals("CD", helper.truncateAInFirst2Positions("ACD"));
     }
 }
