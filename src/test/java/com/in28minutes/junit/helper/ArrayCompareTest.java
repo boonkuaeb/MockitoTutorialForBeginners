4baacb2fdb97de5b8717c2d465ea8d3a2b1d1c16
BOONKUAE BOONSUTTA <boonkuae.boonsutta@scb.co.th>
2020-04-16 12:35:37 +0700

    Unit Testing with Junit: Step 09: Testing Exceptions in JUnit Tests

--- src/test/java/com/in28minutes/junit/helper/ArrayCompareTest.java
+++ src/test/java/com/in28minutes/junit/helper/ArrayCompareTest.java
@@ -5,24 +5,40 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ArrayCompareTest {
 
     @Test
-    void testArraySort_RandomArray()
-    {
+    void testArraySort_RandomArray() {
         // Given
-        int[] number = {12,1,5,2,7};
+        int[] number = {12, 1, 5, 2, 7};
 
         // When
         Arrays.sort(number);
 
         // Assert
-        int[] expected = {1,2,5,7,12};
-        assertArrayEquals(expected,number);
+        int[] expected = {1, 2, 5, 7, 12};
+        assertArrayEquals(expected, number);
         // array contents differ at index [1],
         // expected: <5> but was: <2>
 
+    }
+
+    @Test
+    void testArraySort_NullArray5() {
+        // Given
+        int[] number = null;
+        // When
+        assertThrows(NullPointerException.class,
+                () -> Arrays.sort(number)
+        );
+    }
+
+    @org.junit.Test(expected = NullPointerException.class)
+    public void testArraySort_NullArray4() {
+        // Given
+        int[] number = null;
+        Arrays.sort(number);
     }
 }
